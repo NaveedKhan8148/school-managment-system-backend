7fc1b4689f46d60932d4b091347c398bc58134f7
NaveedKhan <naveedullah088@gmail.com>
2026-04-07 17:34:44 +0500

    stable backend code

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
 
 const connectDB = async () => {
-
-    const conectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
-    console.log(`\n THE DB ARE CONNECTED :${conectionInstance.connection.host}`)
-
-    try { } catch (error) {
-        console.log(`DATABASE ARE NOT CONNECTED`, error)
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
         process.exit(1)
     }
 }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,143 +1,93 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, {Schema} from "mongoose";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      uppercase: true,
-      trim: true,
-      index: true
-    },
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true, 
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
 
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true
     },
-
-    password: {
-      type: String,
-      required: true
-    },
-
-    role: {
-      type: String,
-      enum: ['ADMIN', 'TEACHER', 'STUDENT', 'PARENT'],
-      required: true
-    },
-
-    status: {
-      type: String,
-      enum: ['ACTIVE', 'INACTIVE'],
-      default: 'ACTIVE'
-    },
-
-    refreshToken: {
-      type: String
+    {
+        timestamps: true
     }
-  },
-  {
-    timestamps: true
-  }
 )
 
-userSchema.pre("save", async function () {
-  if (!this.isModified("password")) {
-    return;
-  }
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
 
-  this.password = await bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
 })
 
-userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.password)
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
 }
 
-userSchema.methods.generateAccessToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-      email: this.email,
-      username: this.username,
-    },
-    process.env.ACCESS_TOKEN_SECRET,
-    {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-    }
-  )
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
 }
-
-userSchema.methods.generateRefreshToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-    },
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-    }
-  )
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
 }
 
 export const User = mongoose.model("User", userSchema)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// models/User.model.js (or User.js)
-// import mongoose from 'mongoose';
-
-// const userSchema = new mongoose.Schema({
-//   role: {
-//     type: String,
-//     enum: ['ADMIN', 'TEACHER', 'STUDENT', 'PARENT'],
-//     required: true
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//     lowercase: true,
-//     trim: true
-//   },
-//   password: {
-//     type: String,
-//     required: true
-//   },
-//   status: {
-//     type: String,
-//     enum: ['ACTIVE', 'INACTIVE'],
-//     default: 'ACTIVE'
-//   }
-// }, {
-//   timestamps: {
-//     createdAt: 'created_at',
-//     updatedAt: 'updated_at'
-//   }
-// });
-
-// export const User = mongoose.model('User', userSchema);
-// OR export default
-// export default mongoose.model('User', userSchema);
